c0b470f377992cd3eea657509e3f19e8f8737c2a
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-08-15 16:04:43 +0500

    first commit

---
+++ sum.py
@@ -0,0 +1,5 @@
+first_number = input("Enter Your First Number : ")
+sec_number = input("Enter Your Seconds Number : ")
+
+sum = int(first_number) + int(sec_number)
+print("The sum of two Number is : " + str(sum))
