4949a2e65fc858504c99462a7f59e73f78251276
Abdul Haseeb Imran <98330141+Abdulhaseebimran@users.noreply.github.com>
2022-08-16 10:42:59 +0500

    first commit

---
+++ string1.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+
+
+name = "Abdul Haseeb"
+print(name.upper())
+
+print(name.lower())
+
+print(name.find('Haseeb'))
+print(name.find("abdul"))
+
+print(name.replace("Abdul Haseeb" , "Abdullah"))
+
+print('Abdul' in name)
